14e45180330d8bc6e8efe8b6878bbe662864e30a
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-07-11 23:31:14 +0530

    Added rename notes abilities

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ def link():
     notes_repo.process_notes()
 
 
+@click.command()
+@click.argument('src')
+@click.argument('dst')
+def rename(src, dst):
+    notes_dir_path = os.getcwd()
+    notes_repo = NoteRepo(notes_dir_path)
+    notes_repo.rename_note(src, dst)
+
+
 if __name__ == "__main__":
     cli.add_command(link)
+    cli.add_command(rename)
     cli()
